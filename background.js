c2027ff8a4816cf40d15959be0879a3c1a79fd11
Dillon Murray <dillon.murray@gmail.com>
2017-01-31 17:30:12 -0800

    Styling fixes

--- background.js
+++ background.js
@@ -5,27 +5,34 @@ Assignment: 	Final Project
 *****/
 
 chrome.runtime.onMessage.addListener(function(request) {
-  var message = request.text;
-  if( message.constructor === Array ){
-	  for( var i = 0; i < message.length; i++ ){
-		console.log(message[i]);
-		chrome.downloads.download({"url": message[i]}, function(id){});
+	if (request === null) {
+		return;
 	}
-  } else {
-	  message = message.replace(/>/g, "implying ");
-
-	  var messages = message.split("\n");
-	  var mesCount = messages.length;
-
-	  if( mesCount > 1 ){
-		for( var i = 0; i < mesCount; i++ ) {
-			if( messages[i].valueOf() != "" ) {
-				chrome.tts.speak(messages[i], {'enqueue': true, 'rate': 0.9});
-			}
+	
+	var i = 0,
+	mesCount = 0, 
+	messages = '',
+	message = request.text;
+	
+	if (message.constructor === Arra ) {
+		for (i = 0; i < message.length; i++) {
+			console.log(message[i]);
+			chrome.downloads.download({"url": message[i]}, function(id){});
 		}
-	  } else {
+	} else {
+		message = message.replace(/>/g, "implying ");
+		messages = message.split("\n");
+		mesCount = messages.length;
+
+		if (mesCount > 1) {
+			for (var i = 0; i < mesCount; i++) {
+				if (messages[i].valueOf() !== "") {
+					chrome.tts.speak(messages[i], {'enqueue': true, 'rate': 0.9});
+				}
+			}
+		} else {
 			chrome.tts.speak(message, {'rate': 0.9});
 		}
-  }
-  });
+	}
+});
 
--- content.js
+++ content.js
@@ -7,26 +7,31 @@ Assignment: 	Final Project
 // http://i.imgur.com/6rjsMZN.png 
 
 $(document).ready(function() {
-	var buttonImg = chrome.extension.getURL('microphone.png');
-	if( window.location.href.indexOf("4chan") > -1) {
-		$( ".postInfo" ).after( '<span><img class="ttsButton" src="http://i.imgur.com/6rjsMZN.png" /></span>' );
-		$( ".navLinks.desktop" ).append( ' [<a class="downAll">Get Images</a>]' );
-	} else if( window.location.href.indexOf("youtube") > -1 ) {
-		$( ".wO" ).after( '<div><img class="ttsButton" src="http://i.imgur.com/6rjsMZN.png" /></div>' );
+	var buttonImg = chrome.extension.getURL('microphone.png'),
+	chanImageHtml = '<span><img class="ttsButton" src="http://i.imgur.com/6rjsMZN.png" /></span>',
+	youtubeImageHtml = '<div><img class="ttsButton" src="http://i.imgur.com/6rjsMZN.png" /></div>';
+	
+	if (window.location.href.indexOf("4chan") > -1) {
+		$(".postInfo").after(chanImageHtml);
+		$(".navLinks.desktop").append(' [<a class="downAll">Get Images</a>]');
+	} else if (window.location.href.indexOf("youtube") > -1) {
+		$(".wO").after(youtubeImageHtml);
 	};
 });
 
 
 $(document).on('click', ".ttsButton", function() {
-	var textNode = $( this ).parent().parent().children("blockquote");
-	var message = textNode.clone().children("br").replaceWith("\n").end();
-	message = message.children().remove("a").end().text();
-	chrome.runtime.sendMessage({text: message}, function() {});
+	var me = this,
+	textNode = $(me).parent().parent().children("blockquote"),
+	message = textNode.clone().children("br").replaceWith("\n").end(),
+	cleanedMessage = message.children().remove("a").end().text();
+	
+	chrome.runtime.sendMessage({text: cleanedMessage}, function() {});
 });
 
 $(document).on('click', ".downAll", function() {
 	var URLs = [];
-	$( ".fileThumb" ).each(function() {
+	$(".fileThumb").each(function() {
 		console.log($( this ).attr("href").replace(/\/\//, "http://"));
 		URLs.push($( this ).attr("href").replace(/\/\//, "http://"));
 	});
